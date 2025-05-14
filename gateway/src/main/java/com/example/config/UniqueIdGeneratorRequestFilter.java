aae7a783f7bb7d679480041bb668de30418b052a
Adam Szałański <adsn@softwaremind.com>
2025-05-14 21:13:07 +0200

    refactor: modyfikacja filtru dodającego header X-Unique-Id do requestu na filtr globalny

---
+++ gateway/src/main/java/com/example/config/UniqueIdGeneratorRequestFilter.java
@@ -0,0 +1,38 @@
+package com.example.config;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Component
+public class UniqueIdGeneratorRequestFilter implements GlobalFilter, Ordered {
+
+    private static final String UNIQUE_ID_HEADER = "X-Unique-Id";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        if (!request.getHeaders().containsKey(UNIQUE_ID_HEADER)) {
+            String uniqueId = UUID.randomUUID().toString();
+            ServerHttpRequest modifiedRequest = request
+                    .mutate()
+                    .header(UNIQUE_ID_HEADER, uniqueId)
+                    .build();
+            return chain.filter(exchange.mutate().request(modifiedRequest).build());
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
